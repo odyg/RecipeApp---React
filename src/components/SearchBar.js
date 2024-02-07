a356318a2f43b69c7060f22d9676fc51a409afa1
odyg <100243742+odyg@users.noreply.github.com>
2024-02-07 07:48:23 -0700

    more changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,12 +12,18 @@ function SearchBar({ onSearch }) {
   return (
     <form onSubmit={handleSubmit}>
       <input
+        className="form-control"
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Search recipes..."
+        aria-describedby="button-addon2"
       />
-      <button type="submit">Search</button>
+      <button type="submit" className="btn btn-outline-secondary">
+        Search
+      </button>
+      {/* <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button> */}
     </form>
   );
 }
